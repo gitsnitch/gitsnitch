7bcb61c17f7c2d03d12edc1fbc3eac17e24a4749
Thady Condon <thadycondon@hotmail.com>
2016-08-31 12:30:15 +0100

    Octokit query working with selected repos and returning results.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,31 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     token = request.env["omniauth.auth"]["credentials"]["token"]
-    client = Octokit::Client.new
-    client.access_token = token
+    @username = request.env["omniauth.auth"]["info"]["nickname"]
+    @client = Octokit::Client.new
+    @client.access_token = token
+    repo_collect
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+  end
+
+  def repo_collect
+    data = Array.new
+    @client.repositories.each do |repo|
+      data << repo.name
+    end
+    code_search
+  end
+
+  def code_search
+    results = @client.search_code("facebook user:#{@username}")
   end
 
   def failure
